a3fe5460e07ace872ea3ab25bd4fb8998790381b
Tom Cox <thomasxcox731@gmail.com>
2019-10-16 23:30:40 -0500

    Initial sketeton of public.js written.

---
+++ public/assets/JS/public.js
@@ -0,0 +1,47 @@
+$(function (){
+//when devour button is clicked...
+//Send the PUT request
+$("#change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+      devoured: newDevour
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed sleep to", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+//Form submission--what is this new info we're sending back to the database
+$(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: $("[name=devour]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
