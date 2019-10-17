ec125104db68b5fdeb8fea6471ff5cb638e67711
Tom Cox <thomasxcox731@gmail.com>
2019-10-16 21:24:44 -0500

    Initial file structure created

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(55) NOT NULL,
+  devoured boolean,
+  PRIMARY KEY (id)
+);
